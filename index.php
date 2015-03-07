9deaa1c15f48c5c546756fcd094930511438304d
nabilkhoury <nabilkhoury2001@yahoo.com>
2015-03-07 17:30:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,14 @@ google my name is nabil by direct printout
 <?php
 
 
+phpinfo();
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+phpinfo(INFO_MODULES);
+
+echo $_SERVER['SERVER_NAME'];
+
 echo "my name is nabil from echo code";
 
 ?>
